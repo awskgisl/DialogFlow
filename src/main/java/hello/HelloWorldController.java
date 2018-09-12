b578610b1d8a39cddd62b1c794be92067e8eadf0
awskgisl <43135663+awskgisl@users.noreply.github.com>
2018-09-12 12:28:49 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -2,9 +2,9 @@ package hello;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +12,8 @@ public class HelloWorldController {
 
 	@SuppressWarnings("unused")
 	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
-	public ResponseEntity<SimpleResponse> getResult(@RequestParam String obj) {
-		SimpleResponse emp = new SimpleResponse("Hello " + obj + " Welcome to REST App");
+	public ResponseEntity<SimpleResponse> getResult(@RequestBody RequestData obj) {
+		SimpleResponse emp = new SimpleResponse("Hello " + obj.getObj() + " Welcome to REST App");
 		if (emp == null) {
 			return new ResponseEntity<SimpleResponse>(emp, HttpStatus.NO_CONTENT);
 		}
