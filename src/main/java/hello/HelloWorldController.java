4215eadf0c26fcbdaf1627e332021a2da7dc4121
awskgisl <43135663+awskgisl@users.noreply.github.com>
2018-09-12 12:09:20 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -1,20 +1,22 @@
 package hello;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/webhook")
+@RestController
 public class HelloWorldController {
 
-	@RequestMapping(method = RequestMethod.POST)
-	public @ResponseBody SimpleResponse webhook(@RequestBody String obj){
-
-		System.out.println(obj);
-
-		return new SimpleResponse("Hello " + obj +" Welcome to REST App");
+	@SuppressWarnings("unused")
+	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
+	public ResponseEntity<SimpleResponse> getResult(@RequestBody RequestData obj) {
+		SimpleResponse emp = new SimpleResponse("Hello " + obj.getText() + " Welcome to REST App");
+		if (emp == null) {
+			return new ResponseEntity<SimpleResponse>(emp, HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<SimpleResponse>(emp, HttpStatus.OK);
 	}
 }
