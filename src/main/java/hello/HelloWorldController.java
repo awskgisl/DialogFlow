3d8bb733ccef60989e70d792c3a8a5954c7afeea
awskgisl <43135663+awskgisl@users.noreply.github.com>
2018-09-12 12:22:19 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -13,7 +13,7 @@ public class HelloWorldController {
 	@SuppressWarnings("unused")
 	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
 	public ResponseEntity<SimpleResponse> getResult(@RequestBody RequestData obj) {
-		SimpleResponse emp = new SimpleResponse("Hello " + obj.getText() + " Welcome to REST App");
+		SimpleResponse emp = new SimpleResponse("Hello " + obj.getObj() + " Welcome to REST App");
 		if (emp == null) {
 			return new ResponseEntity<SimpleResponse>(emp, HttpStatus.NO_CONTENT);
 		}
