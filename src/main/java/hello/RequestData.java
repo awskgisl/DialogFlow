a5b4a322dccad18910aa0fb20c57d025497818f5
awskgisl <43135663+awskgisl@users.noreply.github.com>
2018-09-12 12:09:57 +0530

    Create RequestData.java

---
+++ src/main/java/hello/RequestData.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class RequestData {
+	
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
