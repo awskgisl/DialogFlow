86a931921f666d68142d1407343313e237192720
awskgisl <43135663+awskgisl@users.noreply.github.com>
2018-10-09 14:57:58 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -21,47 +21,9 @@ public class HelloWorldController {
 	@RequestMapping(value = "/webhook", method = RequestMethod.GET)
 	public ResponseEntity<SimpleResponse> getResult(@RequestParam String data) {
 
-		String outAll="";
-		try {
+		
 
-			URL url = new URL("https://demo.kgfsl.com/CIP_DX_ENGINE_API/api/accountcollection?id=81");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
-
-			String input="[{\"id\": 1,\"message\":\"petrik\"},{\"id\": 2,\"message\":\"Dan\"}]";
-
-			OutputStream os = conn.getOutputStream();
-			os.write(input.getBytes());
-			os.flush();
-			//os.close();
-			System.out.println(conn.getResponseCode());
-
-			if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-				throw new RuntimeException("Failed : HTTP error code : "
-						+ conn.getResponseCode());
-			}
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					(conn.getInputStream())));
-
-			String output;
-
-			System.out.println("Output from Server .... \n");
-			while ((output = br.readLine()) != null) {
-				//				System.out.println(output);
-				outAll+=outAll+output;
-			}
-			System.out.println(outAll);
-
-			conn.disconnect();
-
-		} catch (Exception e){
-			System.out.println(e.getMessage());
-		}
-
-		SimpleResponse emp = new SimpleResponse("Hello DialogFlows Welcomes you to REST App "+outAll);
+		SimpleResponse emp = new SimpleResponse("Hello DialogFlows Welcomes you to REST App ");
 		if (emp == null) {
 			return new ResponseEntity<SimpleResponse>(emp, HttpStatus.NO_CONTENT);
 		}
