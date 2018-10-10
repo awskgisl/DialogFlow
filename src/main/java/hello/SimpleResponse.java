cb1c09d0cf5c402b98b90f2e263a00d69c369d07
awskgisl <43135663+awskgisl@users.noreply.github.com>
2018-10-10 17:09:46 +0530

    Update SimpleResponse.java

--- src/main/java/hello/SimpleResponse.java
+++ src/main/java/hello/SimpleResponse.java
@@ -1,17 +1,23 @@
 package hello;
 
 public class SimpleResponse {
-    private final String fulfillmentText;
+	private int art_id;
+	private String art_message;
 
-   public SimpleResponse(String fulfillmentText) {
-        this.fulfillmentText = fulfillmentText;
-        
-    }
+	public int getArt_id() {
+		return art_id;
+	}
 
-    public String getFulfillmentText() {
-        return fulfillmentText;
-    }
+	public void setArt_id(int art_id) {
+		this.art_id = art_id;
+	}
 
+	public String getArt_message() {
+		return art_message;
+	}
 
-   
+	public void setArt_message(String art_message) {
+		this.art_message = art_message;
+	}
+
 }
