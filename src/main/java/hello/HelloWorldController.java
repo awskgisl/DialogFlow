c930b775a6cbf0f620a6bf84da44054a04dab4e4
awskgisl <43135663+awskgisl@users.noreply.github.com>
2018-10-29 17:05:24 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -4,8 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.util.List;
 import java.net.URL;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,51 +17,276 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
 	@RequestMapping(value = "/webhook", method = RequestMethod.GET)
-	public String getResult(@RequestParam int ssn, int accnumber,int consnumber, int opttype) {
+	public String getResult(@RequestParam int ssn, int accnumber, int consnumber, int opttype) {
 
 		String outAll = "";
-		try {
-			URL url = new URL("https://demo.kgfsl.com/CIP_DX_ENGINE_API/api/accountcollection?id=81");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
-			String input = "[{\"ssn\":"+ssn+",\"accnumber\":"+accnumber+",\"consnumber\":"+consnumber+",\"opttype\":"+opttype+"}]";
-			System.out.println(input);
-			OutputStream os = conn.getOutputStream();
-			os.write(input.getBytes());
-			os.flush();
-			// os.close();
-			System.out.println(conn.getResponseCode());
 
-			if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+		if (opttype == 1) {
+
+			try {
+				URL url = new URL("https://demo.kgfsl.com/CIP_DX_ENGINE_API/api/accountcollection?id=81");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setDoOutput(true);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/json");
+				String input = "[{\"ssn\":" + ssn + ",\"accnumber\":" + accnumber + ",\"consnumber\":" + consnumber
+						+ ",\"opttype\":" + opttype + "}]";
+				System.out.println(input);
+				OutputStream os = conn.getOutputStream();
+				os.write(input.getBytes());
+				os.flush();
+				// os.close();
+				System.out.println(conn.getResponseCode());
+
+				if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+					throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+				}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+				String output;
+
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					// System.out.println(output);
+					outAll += outAll + output;
+				}
+				System.out.println(outAll);
+
+				conn.disconnect();
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			if (outAll.length() == 0) {
+				return "Empty";
 			}
 
-			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+		}
 
-			String output;
+		else if (opttype == 2) {
 
-			System.out.println("Output from Server .... \n");
-			while ((output = br.readLine()) != null) {
-				// System.out.println(output);
-				outAll += outAll + output;
+			try {
+				URL url = new URL("https://demo.kgfsl.com/CIP_DX_ENGINE_API/api/accountcollection?id=81");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setDoOutput(true);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/json");
+				String input = "[{\"ssn\":" + ssn + ",\"accnumber\":" + accnumber + ",\"consnumber\":" + consnumber
+						+ ",\"opttype\":" + opttype + "}]";
+				System.out.println(input);
+				OutputStream os = conn.getOutputStream();
+				os.write(input.getBytes());
+				os.flush();
+				// os.close();
+				System.out.println(conn.getResponseCode());
+
+				if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+					throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+				}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+				String output;
+
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					// System.out.println(output);
+					outAll += outAll + output;
+				}
+				System.out.println(outAll);
+
+				conn.disconnect();
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
 			}
-			System.out.println(outAll);
+			if (outAll.length() == 0) {
+				return "Empty";
+			}
 
-			conn.disconnect();
+		}
 
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+		else if (opttype == 3) {
+
+			try {
+				URL url = new URL("https://demo.kgfsl.com/CIP_DX_ENGINE_API/api/accountcollection?id=81");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setDoOutput(true);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/json");
+				String input = "[{\"ssn\":" + ssn + ",\"accnumber\":" + accnumber + ",\"consnumber\":" + consnumber
+						+ ",\"opttype\":" + opttype + "}]";
+				System.out.println(input);
+				OutputStream os = conn.getOutputStream();
+				os.write(input.getBytes());
+				os.flush();
+				// os.close();
+				System.out.println(conn.getResponseCode());
+
+				if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+					throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+				}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+				String output;
+
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					// System.out.println(output);
+					outAll += outAll + output;
+				}
+				System.out.println(outAll);
+
+				conn.disconnect();
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			if (outAll.length() == 0) {
+				return "Empty";
+			}
+
 		}
-		if (outAll.length() == 0) {
-			return "Empty";
+
+		else if (opttype == 4) {
+
+			try {
+				URL url = new URL("https://demo.kgfsl.com/CIP_DX_ENGINE_API/api/accountcollection?id=81");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setDoOutput(true);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/json");
+				String input = "[{\"ssn\":" + ssn + ",\"accnumber\":" + accnumber + ",\"consnumber\":" + consnumber
+						+ ",\"opttype\":" + opttype + "}]";
+				System.out.println(input);
+				OutputStream os = conn.getOutputStream();
+				os.write(input.getBytes());
+				os.flush();
+				// os.close();
+				System.out.println(conn.getResponseCode());
+
+				if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+					throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+				}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+				String output;
+
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					// System.out.println(output);
+					outAll += outAll + output;
+				}
+				System.out.println(outAll);
+
+				conn.disconnect();
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			if (outAll.length() == 0) {
+				return "Empty";
+			}
+
+		} else if (opttype == 5) {
+
+			try {
+				URL url = new URL("https://demo.kgfsl.com/CIP_DX_ENGINE_API/api/accountcollection?id=81");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setDoOutput(true);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/json");
+				String input = "[{\"ssn\":" + ssn + ",\"accnumber\":" + accnumber + ",\"consnumber\":" + consnumber
+						+ ",\"opttype\":" + opttype + "}]";
+				System.out.println(input);
+				OutputStream os = conn.getOutputStream();
+				os.write(input.getBytes());
+				os.flush();
+				// os.close();
+				System.out.println(conn.getResponseCode());
+
+				if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+					throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+				}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+				String output;
+
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					// System.out.println(output);
+					outAll += outAll + output;
+				}
+				System.out.println(outAll);
+
+				conn.disconnect();
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			if (outAll.length() == 0) {
+				return "Empty";
+			}
+
 		}
+
+		else if (opttype == 6) {
+
+			try {
+				URL url = new URL("https://demo.kgfsl.com/CIP_DX_ENGINE_API/api/accountcollection?id=81");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setDoOutput(true);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/json");
+				String input = "[{\"ssn\":" + ssn + ",\"accnumber\":" + accnumber + ",\"consnumber\":" + consnumber
+						+ ",\"opttype\":" + opttype + "}]";
+				System.out.println(input);
+				OutputStream os = conn.getOutputStream();
+				os.write(input.getBytes());
+				os.flush();
+				// os.close();
+				System.out.println(conn.getResponseCode());
+
+				if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+					throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+				}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+				String output;
+
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					// System.out.println(output);
+					outAll += outAll + output;
+				}
+				System.out.println(outAll);
+
+				conn.disconnect();
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			if (outAll.length() == 0) {
+				return "Empty";
+			}
+
+		}
+
+		else {
+			outAll = "Please enter a valid Option Type";
+		}
+
 		return outAll;
+
 	}
 
 	@RequestMapping(value = "/postwebhook", method = RequestMethod.POST)
-	public SimpleResponse getResultBody(@RequestBody  List<SimpleResponse> data) {
+	public SimpleResponse getResultBody(@RequestBody List<SimpleResponse> data) {
 		SimpleResponse response = new SimpleResponse();
 		response.setId(data.get(0).getId());
 		response.setMessage(data.get(0).getMessage());
